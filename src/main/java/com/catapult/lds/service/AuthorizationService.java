68b4eed400237e96c304d16006eeb95ee1b82275
jayadeep gilroy <jayadeep.gilroy@catapultsports.com>
2022-10-03 11:48:52 -0400

    OS-379 removing the unwanted object mapper

--- src/main/java/com/catapult/lds/service/AuthorizationService.java
+++ src/main/java/com/catapult/lds/service/AuthorizationService.java
@@ -9,7 +9,6 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.SignatureVerificationException;
 import com.auth0.jwt.interfaces.DecodedJWT;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.security.interfaces.RSAPublicKey;
 import java.util.Base64;
@@ -18,9 +17,7 @@ import java.util.Base64;
  * {@code AuthorizationService } provides method for validating JWT token
  */
 public class AuthorizationService {
-
-    private static final ObjectMapper mapper = new ObjectMapper();
-
+    
     /**
      * Validates the provided JWT token
      *
